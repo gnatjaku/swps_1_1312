078506ccc6486a378e26cbba3290ce79954733e8
Jakub Gnat <jakub.gnat@gmail.com>
2026-01-22 23:07:27 +0100

    csv

--- test_csv_otwarcie.py
+++ test_csv_otwarcie.py
@@ -1,13 +1,18 @@
 import csv
+import logging
+import sys
 
-# with open("ceny.csv", newline='', encoding="utf-8") as f:
-#     reader = csv.reader(f, delimiter=';')
-#     for row in reader:
-#         print(row)
-
+logging.basicConfig(
+    level=logging.INFO,
+    stream=sys.stdout,
+    format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+)
 
 sumy = {}
 liczniki = {}
+laczna_wartosc = {}
+
+logger = logging.getLogger("ceny_logger")
 
 with open("ceny.csv", newline='', encoding="utf-8") as f:
     reader = csv.DictReader(f, delimiter=';')
@@ -18,14 +23,14 @@ with open("ceny.csv", newline='', encoding="utf-8") as f:
         cena = float(row['cena'].replace(',', '.'))
 
         sumy[produkt] = sumy.get(produkt, 0) + cena
-
         liczniki[produkt] = liczniki.get(produkt, 0) + 1
-
+        logger.info(f"Produkt: {produkt}, ilość: {ilosc}, cena: {cena} lączna wartość {cena * int(ilosc)}")
+        laczna_wartosc[produkt] = laczna_wartosc.get(produkt, 0) + cena * int(ilosc)
 
 
 with open("ceny3.csv", "w", newline='', encoding="utf-8") as f:
     writer = csv.writer(f, delimiter=';')
-    writer.writerow(["produkt","srednia"])
+    writer.writerow(["produkt","srednia","łączna_wartość"])
     for produkt in sumy:
         srednia = round(sumy[produkt] / liczniki[produkt], 2)
-        writer.writerow([produkt,srednia])
+        writer.writerow([produkt,srednia,laczna_wartosc[produkt]])
